e8f22b1e14b7ab6ec53839556460ff557843533c
fwong <fswong@gmail.com>
2015-09-30 19:16:06 -0700

    Initial commit

---
+++ melon_raffle.py
@@ -0,0 +1,17 @@
+from customer_info import organize_customer_data
+from random import choice
+
+def pick_winner(customers):
+    """Choose a random winner from list of customers."""
+
+    chosen_customer = choice(customers)
+
+    print "Contact {name} at {email} to notify them they've won".format(
+        name=chosen_customer.name,
+        email=chosen_customer.email
+        )
+
+
+customers = organize_customer_data("customers.txt")
+
+pick_winner(customers)
